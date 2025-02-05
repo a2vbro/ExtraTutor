82946dba7292f14fbc66cbe0cdad01a20c740db0
Oleksandr Akhtyrskyi <aakht@softserveinc.com>
2025-02-05 09:09:38 +0100

    added stub for web

--- etui/src/App.js
+++ etui/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {RootNavigation, SCREENS} from '../services';
+//import {RootNavigation, SCREENS} from '../services';
+import {Text, View} from 'react-native';
 
 export default function App() {
 
-    return (
-      <RootNavigation initialRouteName={SCREENS.CONNECT_WALLET} />
-    );
+    // return (
+    //   <RootNavigation initialRouteName={SCREENS.CONNECT_WALLET} />
+    // );
+
+    return (<View><Text>Web content</Text></View>);
 }
