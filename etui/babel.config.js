354660fcf9dc4d2196d5e93998e1b78e4475c4a3
Oleksandr Akhtyrskyi <aakht@softserveinc.com>
2025-02-05 10:56:41 +0100

    fix babel config

--- etui/babel.config.js
+++ etui/babel.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return  {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      '@babel/plugin-transform-class-static-block',
+      'react-native-reanimated/plugin',
+      'transform-inline-environment-variables',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.tsx', '.jsx', '.js', '.ts'],
+
+          alias: {
+            '@components': ['./components'],
+            '@services': ['./services'],
+            '@mocks': ['./mocks'],
+          },
+        },
+      ],
+    ],
+  };
 };
